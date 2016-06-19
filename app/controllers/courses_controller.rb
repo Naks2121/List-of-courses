c772542f637fc943d74fe1aeb488948cfbca08e4
Roman Semeniuk <alonsit316@yandex.ua>
2016-06-19 16:14:13 +0300

    use coursera API

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   def index
-  	@search_term = params[:looking_for] || 'jhu'
+  	@search_term = params[:looking_for] || 'programming'
   	@courses = Coursera.for(@search_term)
   end
 end
